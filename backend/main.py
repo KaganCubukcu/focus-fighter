a07ddd95c4b416dc92daeb30819764916440a486
Kagan Cubukcu <kagancancubukcu@gmail.com>
2025-12-09 10:31:07 +0300

    fix: update cors for production

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import game
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="Focus Fighter API")
 
 # CORS Configuration
 origins = [
-    "http://localhost:3000",  # Next.js Frontend
+    "http://localhost:3000",  # Next.js Frontend (Local)
+    os.getenv("FRONTEND_URL"),
+    "*",
 ]
 
 app.add_middleware(
